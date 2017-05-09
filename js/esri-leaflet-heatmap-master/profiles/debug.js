bcaf1f29644642cc4810ac20476b56fbeb7bdc00
Mayflya <albin.lundberg@gmail.com>
2017-05-09 13:27:40 +0200

    Rensat och allt fungerar

--- js/esri-leaflet-heatmap-master/profiles/debug.js
+++
@@ -1,6 +0,0 @@
-import config from './base.js';
-
-config.dest = 'dist/esri-leaflet-heatmap-debug.js';
-config.sourceMap = 'inline';
-
-export default config;
--- js/main.js
+++ js/main.js
@@ -1,42 +1,25 @@
-        
-function marker(latitude,longitude,popupData,style)
+//Konstruktor för marker object    
+function marker(latitude,longitude,popupData,color,icon,shape,rowIndex)
 {
   this.latitude=latitude;
   this.longitude=longitude;
   this.popupData=popupData;
-  this.style = style;
+  this.color= color;
+  this.icon = icon;
+  this.shape = shape;
+  this.rowIndex = rowIndex
 }
 
-var marker1 = new marker(59.96,17.62,"AO-no:1 Status: Påbörjad","yellow");
-var marker2 = new marker(59.75,17.75,"AO-no:2 Status: Ej påbörjad","red");
-var marker3 = new marker(59.75,17.39,"AO-no:3 Status: Klar","green");
-var marker4 = new marker(60.10,17.13,"AO-no:4 Status: Påbörjad","yellow");
-        
-var markers = [marker1,marker2,marker3,marker4];
-        
-function mapLayers(esriBasemap,esriFeatureLayer)
+//Funktion för att välja önskat baslager
+function mapLayers(basemap,featureLayer)
 {
-  this.esriBasemap=esriBasemap;
-  this.esriFeatureLayer=esriFeatureLayer;
+  this.basemap=basemap;
+  this.featureLayer=featureLayer;
 }
 
-// Förändra så funktionen tar värde för baskarta och feature-länk från Flow
-var mapData = new mapLayers("Streets",
-"https://gisapp.msb.se/arcgis/rest/services/Raddningstjanst/Brandstationer/FeatureServer/0");
-
-var map = new L.map('map');
-
-var locate = L.control.locate({
-  flyTo:true,
-  locateOptions:{
-    setView:'false',
-    maxZoom: 13
-  }
-}).addTo(map);
-
-locate.start();
-
-var iconize = function (symbol, color, shape) {
+//Skapar ikon
+function createIcon(symbol, color, shape)
+{
   var icon = L.ExtraMarkers.icon({
     icon: symbol,
     markerColor: color,
@@ -47,34 +30,60 @@ var iconize = function (symbol, color, shape) {
   return icon;
 }
 
-var layer = L.esri.basemapLayer(mapData.esriBasemap).addTo(map);
-var markerGroup = L.featureGroup();
+//Skapar fiktiv data
+var marker1 = new marker(59.96,17.62,"AO-no:1 Status: Påbörjad","yellow","Novacura Gubbe","square",1);
+var marker2 = new marker(59.75,17.75,"AO-no:2 Status: Ej påbörjad","red","Novacura Gubbe","square",2);
+var marker3 = new marker(59.75,17.39,"AO-no:3 Status: Klar","green","Novacura Gubbe","square",3);
+var marker4 = new marker(60.10,17.13,"AO-no:4 Status: Påbörjad","yellow","Novacura Gubbe","square",4);
 
+//Skapar mapLayers objekt
+var mapData = new mapLayers("http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}","https://gisapp.msb.se/arcgis/rest/services/Raddningstjanst/Brandstationer/FeatureServer/0");
+
+//Array med markers        
+var markers = [marker1,marker2,marker3,marker4];
+
+//Variabler som används i markörerna
+var mark,row;        
+//Instansierar kartan
+var map = new L.map('map');
+//Adderar kontrol för att visa egen position
+var locate = L.control.locate({
+  flyTo:true,
+  locateOptions:{
+    setView:'false',
+    maxZoom: 13
+  }
+}).addTo(map);
+//Startar lokaliseringen vid appstart
+locate.start();
+//Adderar baskarta till map
+var layer = L.tileLayer(mapData.basemap).addTo(map);
+//Skapar featuregroup och lägger till click-event
+var markerGroup = L.featureGroup().addTo(map).on('click',selectedMarker);
+//Array för att zooma vyn till markörerna
 var markerBounds = [];
 
-markers.forEach(function (m) {
-  //Parameters 
-  var mark = L.marker([m.latitude, m.longitude], {icon: iconize('glyphicon-wrench', m.style, 'square')}).addTo(markerGroup);
+//Skapar markörer
+markers.forEach(function (m) 
+{
+  mark = L.marker([m.latitude, m.longitude], {icon: createIcon('glyphicon-wrench', m.color, m.shape)}).addTo(markerGroup);
   mark.bindPopup(m.popupData);
+  mark.row = m.rowIndex;
   markerBounds.push([m.latitude, m.longitude])
 });
 
 //Flyttar View så man ser alla punkter i markers-lagret
 map.fitBounds(markerBounds);
 
-//Clustering
+//Klustrering av markörerna
 var clusterMarkers= L.markerClusterGroup();
-
-L.esri.Cluster.featureLayer({
-  url: mapData.esriFeatureLayer,
-  pointToLayer: function (geojson, latlng) {
-    return L.marker(latlng, {
-      icon: iconize('glyphicon-fire', 'red', 'circle')
-    })
-  }
-}).addTo(markerGroup);
-
 clusterMarkers.addLayer(markerGroup);
 clusterMarkers.addTo(map);
+
+//Kod för att välja markörer
+ function selectedMarker(event){
+    console.log(event.layer.row);
+}
+
 
 
