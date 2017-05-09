37af9dfd792b7f767a9da41d3d8b6e515d4da0be
jonas-g <jonas.gabrielson@me.com>
2017-05-09 14:45:31 +0200

    Ny popup igen
    Ändrade från att använda JSON-objekt till att skapa en enklare struktur
    som utgår från att dela upp en sträng med split().

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 //Konstruktor för marker object    
 function marker(latitude,longitude,popupData,color,icon,shape,rowIndex)
 {
-  var json = JSON.parse('{' + popupData + '}');
   this.latitude=latitude;
   this.longitude=longitude;
-  this.popupData="<b>" + json.Namn + "</b><br/>Arbetsorder: " + json.AO + "<br/>Status: " + json.Status;
+  this.popupData = popupData;
   this.color= color;
   this.icon = icon;
   this.shape = shape;
   this.rowIndex = rowIndex
+}
+
+//Formaterar en inputsträng som är skriven som Key-Value-par till en lista
+function formatPopupData(value){
+  var popupContent = "";
+  var textarray = value.split(',');
+  textarray.forEach(function (p){
+    keyValuePair = p.split(':');
+    popupContent += "<b>" + keyValuePair[0] + ":</b> " + keyValuePair[1] + "<br/>";
+  })
+  return popupContent;
 }
 
 //Funktion för att välja önskat baslager
@@ -32,10 +42,10 @@ function createIcon(symbol, color, shape)
 }
 
 //Skapar fiktiv data
-var marker1 = new marker(59.96,17.62,'"AO":1, "Namn":"Reparation av radiomast", "Status": "Påbörjad"',"yellow","Novacura Gubbe","square",1);
-var marker2 = new marker(59.75,17.75,'"AO":2, "Namn":"Kabelfel", "Status": "Ej påbörjad"',"red","Novacura Gubbe","square",2);
-var marker3 = new marker(59.75,17.39,'"AO":3, "Namn":"Död råtta", "Status": "Klar"',"green","Novacura Gubbe","square",3);
-var marker4 = new marker(60.10,17.13,'"AO":4, "Namn":"Födelsedagskalas", "Status": "Påbörjad"',"yellow","Novacura Gubbe","square",4);
+var marker1 = new marker(59.96,17.62,'AO:1, Namn:Reparation av radiomast, Status:Påbörjad',"yellow","Novacura Gubbe","square",1);
+var marker2 = new marker(59.75,17.75,'AO:2, Namn:Kabelfel, Status:Ej påbörjad',"red","Novacura Gubbe","square",2);
+var marker3 = new marker(59.75,17.39,'AO:3, Namn:Död råtta, Status:Klar',"green","Novacura Gubbe","square",3);
+var marker4 = new marker(60.10,17.13,'AO:4, Namn:Födelsedagskalas, Status:Påbörjad',"yellow","Novacura Gubbe","square",4);
 
 //Skapar mapLayers objekt
 var mapData = new mapLayers("http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}","https://gisapp.msb.se/arcgis/rest/services/Raddningstjanst/Brandstationer/FeatureServer/0");
@@ -68,7 +78,7 @@ var markerBounds = [];
 markers.forEach(function (m) 
 {
   mark = L.marker([m.latitude, m.longitude], {icon: createIcon('glyphicon-wrench', m.color, m.shape)}).addTo(markerGroup);
-  mark.bindPopup(m.popupData);
+  mark.bindPopup(formatPopupData(m.popupData));
   mark.row = m.rowIndex;
   markerBounds.push([m.latitude, m.longitude])
 });
